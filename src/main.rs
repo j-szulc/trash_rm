b6ebe7a9cfe303b3b6cc3e57e92b5112d990e528
Jakub Szulc <58491724+j-szulc@users.noreply.github.com>
2023-11-01 05:34:05 +0100

    Minimal working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+use trash;
+
+/// A basic example
+#[derive(StructOpt, Debug)]
+#[structopt(name = "trash_rm")]
+struct Opt {
+
+    /// Files to process
+    #[structopt(name = "FILE", parse(from_os_str))]
+    files: Vec<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+    for file in opt.files {
+        let path = file.as_path().as_os_str().to_string_lossy();
+        if !file.exists() {
+            // print error
+            eprintln!("trash_rm: {path}: No such file or directory");
+            continue;
+        }
+        match trash::delete(&file) {
+            Ok(_) => {}
+            Err(e) => {
+                // print error
+                eprintln!("trash_rm: {path}: {e}");
+            }
+        }
+    }
 }
